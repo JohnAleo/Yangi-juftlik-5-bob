57275004706b9d603d45ce851c02a9650b99a225
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-04-22 21:56:12 +0900

    Update 5-bob Lug'at va To'plam.py

--- 5-bob Lug'at va To'plam.py
+++ 5-bob Lug'at va To'plam.py
@@ -108,17 +108,93 @@ Created on Sun Mar 12 18:08:45 2023
 #student['university']='Harvard business school'
 #print(f"{student['name'].title()} {student['age']}, {student['university'].upper()}")
 
-cinema={}
-cinema['name']='titanic'
-cinema['genre']='romance,drama'
-cinema['director']='james cameron'
-cinema['imdb rate']='8.7 out of 10'
-cinema['stars']='leonardo di caprio,kate winslet'
-print(f"{cinema['name'].title()},\
-      {cinema['genre']},\
-          {cinema['director'].title()},\
-             {cinema['imdb rate']},\
-                {cinema['stars'].title()}")
+#cinema={}
+#cinema['name']='titanic'
+#cinema['genre']='romance,drama'
+#cinema['director']='james cameron'
+#cinema['imdb rate']='8.7 out of 10'
+#cinema['stars']='leonardo di caprio,kate winslet'
+#print(f"{cinema['name'].title()},\
+#      {cinema['genre']},\
+#          {cinema['director'].title()},\
+#             {cinema['imdb rate']},\
+#                {cinema['stars'].title()}")
+
+
+# Qiymatni O'zgartirish
+#car={}
+#car['price']=35000
+#car['color']='green'
+#car['company']='chevrolet'
+#print(f"{car['color']}, {car['price']}, {car['company']} $")
+
+#Kalit So'z-Qiymat Juftligini O'chirish
+#car={'company':'Porsche','model':'cayenne','speed':320,'type':'crossover'}
+#print(car)
+#del car['company']
+#print(car)
+
+
+#engineer={'name':'Pol','speciality':'AI engineering','age':'28','company':'google'}
+#print(engineer)
+
+#Amaliyot
+#otam={'ism':'akrom','yosh':55,'kasb':'muhandis','mashinasi':'prado'}
+#print(otam)
+#onam={'ismi':'nargiza','kasbi':'shifokor','biznesi':'meva,gul'}
+#print(onam)
+#opam={'ismi':'Zebiniso','qiziqishi':'dizaynerlik'}
+#print(opam)
+#sevimli_taomlar={}
+#sevimli_taomlar['jahongir']='jahongir xalimga ishqiboz'
+#sevimli_taomlar['zebiniso']='zebiniso uchun manti yaxshi'
+#sevimli_taomlar['akrom']='dadamga tandir yoqadi'
+#sevimli_taomlar['nargiza']='onam oshni yaxshi pishiradi va istemol qiladi'
+#sevimli_taomlar['muzaffar']='muzaffarga shirguruch yoqadi'
+#sevimli_taomlar={'jahongir':'xalim','Zebiniso':'manti','akrom':'tandir','nargiza':'osh',\
+                 #'muzaffar':'shirguruch'}
+#print(f"{sevimli_taomlar['jahongir'].title()}, {sevimli_taomlar['zebiniso'].title()},\
+     # {sevimli_taomlar['akrom'].title()}, {sevimli_taomlar['nargiza'].title()},\
+     #     {sevimli_taomlar['muzaffar'].title()}")
+
+
+
+
+
+dictionary={}
+dictionary['int']="o'zgaruvchining qiymatini butun sonligini ko'rsatadi"
+dictionary['title']='kiritilyotgan qiymatni bosh harfda beradi'
+dictionary['upper']='kiritilayotgan qiymatlarni faqat katta harflarda beradi'
+dictionary['lower']='kiritilayotgan qiymatlarni faqat kichik harflarda beradi'
+dictionary['for']="shart berayotganda uchun ma'nosida keladi"
+dictionary['or']='shart bajarilayotganda 2tadan birortasi bajarilishini anglatadi'
+dictionary['and']='shart bajarilayotganda 2tadan shartning ikkalasi bajarilganda ishlaydi'
+dictionary['float']="integerdan farqli ravishda matnli o'zgaruvchilarni oladi"
+dictionary['sort']='qiymatlarni turlarga ajratadi'
+dictionary['not in']="berilgan qiymatni ro'yxat ichida bo'lmagandagi holatda ishlatiladi"
+
+print(f"{dictionary['int']},\
+      {dictionary['title']},\
+          {dictionary['upper']},\
+              {dictionary['lower']},\
+                  {dictionary['for']},\
+                      {dictionary['or']},\
+                          {dictionary['and']},\
+                              {dictionary['float']},\
+                                  {dictionary['sort']},\
+                                      {dictionary['not in']}")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
