422aec5b4c94c37b62644ccf0a01f12ab136ef76
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-05-06 23:11:44 +0900

    Create 5.2Lug'at elementlari bilan ishlash.py

---
+++ 5.2Lug'at elementlari bilan ishlash.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  6 22:29:43 2023
+
+@author: xudoy
+"""
+
+#Items metodi
+talaba={
+        'ism':"alijon",
+        'familiya':'Shamsiyev',
+        'yosh':22,
+        'fakultet':'matematiha',
+        'kurs':4}
+
+print(talaba.items())
+for kalit, qiymat in talaba.items():
+    print(f"Kalit:{kalit}")
+    print(f"Qiymat:{qiymat}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
