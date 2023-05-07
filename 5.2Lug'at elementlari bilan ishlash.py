3435687877bde19065463576b7650eac8825e9e2
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-05-07 21:35:26 +0900

    Update 5.2Lug'at elementlari bilan ishlash.py

--- 5.2Lug'at elementlari bilan ishlash.py
+++ 5.2Lug'at elementlari bilan ishlash.py
@@ -6,41 +6,61 @@ Created on Sat May  6 22:29:43 2023
 """
 
 #Items metodi
-talaba={
-        'ism':"alijon",
-        'familiya':'Shamsiyev',
-        'yosh':22,
-        'fakultet':'matematiha',
-        'kurs':4}
+#talaba={
+#        'ism':"alijon",
+#        'familiya':'Shamsiyev',
+#        'yosh':22,
+#        'fakultet':'matematiha',
+#        'kurs':4}
 
-print(talaba.items())
-for kalit, qiymat in talaba.items():
-    print(f"Kalit:{kalit}")
-    print(f"Qiymat:{qiymat}")
-
-
+#print(talaba.items())
+#for kalit, qiymat in talaba.items():
+#    print(f"Kalit:{kalit}")
+#    print(f"Qiymat:{qiymat}")
 
 
-
-
-
-
-
-
-
-
-
-
+#mashinalar={
+ #   'bmw':'X6',
+ #   'mercedes':'maybach',
+ #   'bentley':'continentel',
+ #   'lamborghini':'urus',
+  #  'chevrolet':'camaro',
+  #  'ford':'mustang'}
+#for kalit, qiymat in mashinalar.items():
+ #   print(f"Kalit:{kalit}")
+  #  print((f"Qiymat:{qiymat}"))
 
 
 
+#ovqatlar={
+#    'osh':'Uzbekistan',
+#    'sushi':'Yaponiya',
+#    'kimpap':'Koreya',
+#    'beshbarmoq':'Kazakistan',
+#    'fish and chips':'England'}
+#for kalit, qiymat in ovqatlar.items():
+#    print(f"Kalit: {kalit}")
+#    print(f"Qiymat:{qiymat}")
 
 
+#cars={
+#      'bmw':'Germany',
+#      'GM':'USA',
+#      'Pigeot':'France',
+#      'Toyota':'Japan',
+#      'hyundai':'Korea'}
+#for rusum, davlat in cars.items(): 
+#    print(f"Rusum,{rusum}")
+#    print(f"Davlat,{davlat}")
 
 
-
-
-
+materials={
+    'polyster':'sintetika',
+    'cotton':'paxta',
+    'rubber':'rezina'}
+for material, tarjima in materials.items():
+    print(f"material,{material}")
+    print(f"tarjima,{tarjima}")
 
 
 
