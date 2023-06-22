bb3620881017924621282ce86f0888efeb9be72f
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-06-22 21:37:35 +0900

    Create 5.3 To'plamlar.py

---
+++ 5.3 To'plamlar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 20:50:10 2023
+
+@author: xudoy
+"""
+
+sonlar={1,2,3}
+print(sonlar)
+
+ismlar={'Azamat','laziz','javohir'}
+print(ismlar)
+
+sonlar1={4,4,1,1,1,1,3,5,45,48,78,95,95}
+print(sonlar1)
