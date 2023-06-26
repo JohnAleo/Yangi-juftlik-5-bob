ca4cf9d53782ed3d2b753217b8c5abd699a8839c
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-06-26 21:11:24 +0900

    Update 5.3 To'plamlar.py

--- 5.3 To'plamlar.py
+++ 5.3 To'plamlar.py
@@ -30,19 +30,87 @@ Created on Thu Jun 22 20:50:10 2023
 # Bir nechta element qo'shish uchun update metodidan foydalaniladi
 
 
-mevalar={'anjir', 'olma','uzum'}
-mevalar.add('banan')
+#mevalar={'anjir', 'olma','uzum'}
+#mevalar.add('banan')
+#print(mevalar)
+#mevalar.update(['ananas','kiwi'])
+#print(mevalar)
+
+#car={'bmw','nexia','porsche'}
+#car.update(['supra','fiat','camaro'] )
+#print(car)
+
+#fruits={'apple','banana','melon','peach'}
+#fruits.add('lime')
+#print(fruits)
+
+
+#phone={'iphone','samsung','nokia'}
+#phone.update(['sony','goggle','sm'])
+#print(phone)
+
+laptops={'hp','samsung','mac'}
+laptops.add('imac')
+print(laptops)
+
+
+# TO'PLAMDAN ELEMENT O'CHIRISH
+# To'plamdan element o'chirish uchun .discard() va .remove() metodlaridan 
+#foydalanamiz
+
+mevalar={'anjir','uzum','anor','banan','olcha','apelsin','gilos'}
+mevalar.remove('uzum')
 print(mevalar)
-mevalar.update(['ananas','kiwi'])
+mevalar.discard('banan')
 print(mevalar)
 
-car={'bmw','nexia','porsche'}
-car.update(['supra','fiat','camaro'] )
-print(car)
+#daraxt={'terak','tol','jiyda','olma','mahogany','popplar','oak','eman'}
+#daraxt.remove('majnuntol') # remove metodi ro'yxatda yo'q qiymat bo'ganda Key error chiqaradi
+#daraxt.discard('tol')
+#daraxt.discard('pooplar')
+#daraxt('bodom')
+#print(daraxt)
 
-fruits={'apple','banana','melon','peach'}
-fruits.add('lime')
-print(fruits)
+sonlar={1,2,3,4,5,6,7,89,87,54,65,24,28,36,9,7,4,52,6,5,2,4,45,522,3,36}
+son=sonlar.pop()
+print(son) # pop elementi to'plamdan ixtiyoriy elementni sug'urib oladi chunki indeks aniq bo'maydi
+print(sonlar)
+
+products={'meat','water','onion','banan','bread','juice','sugar','salt'}
+product=products.pop()
+print(product)
+print(products)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
