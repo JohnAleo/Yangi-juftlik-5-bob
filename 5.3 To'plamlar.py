e28aade3fa675a91ae4b5063e1edc46938b6efe8
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-06-28 21:36:02 +0900

    Update 5.3 To'plamlar.py
    yangi

--- 5.3 To'plamlar.py
+++ 5.3 To'plamlar.py
@@ -49,20 +49,20 @@ Created on Thu Jun 22 20:50:10 2023
 #phone.update(['sony','goggle','sm'])
 #print(phone)
 
-laptops={'hp','samsung','mac'}
-laptops.add('imac')
-print(laptops)
+#laptops={'hp','samsung','mac'}
+#laptops.add('imac')
+#print(laptops)
 
 
 # TO'PLAMDAN ELEMENT O'CHIRISH
 # To'plamdan element o'chirish uchun .discard() va .remove() metodlaridan 
 #foydalanamiz
 
-mevalar={'anjir','uzum','anor','banan','olcha','apelsin','gilos'}
-mevalar.remove('uzum')
-print(mevalar)
-mevalar.discard('banan')
-print(mevalar)
+#mevalar={'anjir','uzum','anor','banan','olcha','apelsin','gilos'}
+#mevalar.remove('uzum')
+#print(mevalar)
+#mevalar.discard('banan')
+#print(mevalar)
 
 #daraxt={'terak','tol','jiyda','olma','mahogany','popplar','oak','eman'}
 #daraxt.remove('majnuntol') # remove metodi ro'yxatda yo'q qiymat bo'ganda Key error chiqaradi
@@ -71,17 +71,63 @@ print(mevalar)
 #daraxt('bodom')
 #print(daraxt)
 
-sonlar={1,2,3,4,5,6,7,89,87,54,65,24,28,36,9,7,4,52,6,5,2,4,45,522,3,36}
-son=sonlar.pop()
-print(son) # pop elementi to'plamdan ixtiyoriy elementni sug'urib oladi chunki indeks aniq bo'maydi
-print(sonlar)
+#sonlar={1,2,3,4,5,6,7,89,87,54,65,24,28,36,9,7,4,52,6,5,2,4,45,522,3,36}
+#son=sonlar.pop()
+#print(son) # pop elementi to'plamdan ixtiyoriy elementni sug'urib oladi chunki indeks aniq bo'maydi
+#print(sonlar)
 
-products={'meat','water','onion','banan','bread','juice','sugar','salt'}
-product=products.pop()
-print(product)
-print(products)
+#products={'meat','water','onion','banan','bread','juice','sugar','salt'}
+#product=products.pop()
+#print(product)
+#print(products)
+
+# TO'PLAMLAR USTIDA AMALLAR
+# IKKI TO'PLAMNI BIRLASHTIRISH
+A={1,2,3,4}
+B={3,4,5,6}
+C=A|B
+print(C)
+D=A.union(B)
+print(D)
+
+chevrolet={'lacetti','nexia','suburman','tahoe','equinox'}
+ford={'rapter','taaurus','explorer','shalby'}
+american_cars=chevrolet|ford
+print(american_cars)
+
+hunarmandlar={'kulol','haykaltarosh','usta','stomatolog','rassom','sartarosh'}
+kasb_egalari={'ustoz','dasturchi','stomatolog','dizayner','sartaarosh','shifokor'}
+mehnatga_layoqatlilar=(hunarmandlar|kasb_egalari)
+print(mehnatga_layoqatlilar)
+
+kompyuter={'lg','samsung','microfoft','apple','msi'}
+laptop={'lenovo','apple','sony','hp','samsung','acer'}
+print(kompyuter|laptop)
+
+qiziqchilar={'erik haydar','bravo','million','mirshakar fayzulloyev'}
+reperlar={'shaka','erik haydar','shoxrux','shaxriyor','konsta'}
+sanatkorlar=qiziqchilar|reperlar
+print(sanatkorlar)
+
+kompozitorlar={'bax','motsart','shopenhauer'}
+olimlar={'nyuton','galiley','paskal','amper','eynshteyn'}
+ziyolilar=kompozitorlar|olimlar
+print(ziyolilar)
+
+# IKKI TO'PLAMNING BIR XIL ELEMENTLAINI TOPISH
+goshtli_taomlar={'manti','norin','shashlik','osh','lagmon','qizilcha'}
+xamir_taomlar={'manti','norin','xonim','yupqa','quymoq','lagmon','gilmindi'}
+(goshtli_taomlar&xamir_taomlar)
+print(goshtli_taomlar&xamir_taomlar)
+print(goshtli_taomlar.intersection(xamir_taomlar))
 
 
+A={'olma','uzum','gilos','banan','shaftoli','behi'}
+B={'ananas','behi','kiwi','anjir','banan','qulupnay','mandarin'}
+print(A&B)
+print(A-B)
+print(B-A)
+print(A^B)
 
 
 
