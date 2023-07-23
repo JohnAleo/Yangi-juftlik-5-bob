fb6b41854138f23debe8abbc9e390b61d2d395fe
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-07-23 15:33:26 +0900

    dfv
    ldscmmv

--- LUG'AT ICHIDA RO'YXAT.py
+++ LUG'AT ICHIDA RO'YXAT.py
@@ -7,53 +7,86 @@ Created on Mon Jul 10 20:43:55 2023
 
 #LUG'AT ICHIDA LUG'AT
 
-dasturchilar={
-    'ali':['python',  'c++'],
-    'vali':['html',  'css',  'js'],
-    'hasan':['php',  'sql'],
-    'husan':['python',  'php'],
-    'maryam':['c++',  'c#']
-    }
-for ism,tillar in dasturchilar.items():
-    print(f"\n{ism.title()}:",end='')
-    for til in tillar:
-        print(f'{til.upper()}',end='')
+#dasturchilar={
+#    'ali':['python',  'c++'],
+    #'vali':['html',  'css',  'js'],
+   # 'hasan':['php',  'sql'],
+   # 'husan':['python',  'php'],
+   # 'maryam':['c++',  'c#']
+  #  }
+#for ism,tillar in dasturchilar.items():
+#    print(f"\n{ism.title()}:",end='')
+#    for til in tillar:
+#        print(f'{til.upper()}',end='')
         
         
 #LUG'AT ICHIDA LUG'AT
-hamkasblar={
-    'ali':{'familiya':'valiyev',
-           'tyil':1995,
-           'malumot':'oliy',
-           'tillar':['python','c++']},
+#hamkasblar={
+#    'ali':{'familiya':'valiyev',
+#           'tyil':1995,
+#           'malumot':'oliy',
+#           'tillar':['python','c++']},
     
-    'vali':{'familiya':'aliyev',
-            'tyil':2001 ,
-            'malumot':"o'rta maxsus",
-            'tillar':['html','css','js']},
+#    'vali':{'familiya':'aliyev',
+#            'tyil':2001 ,
+#            'malumot':"o'rta maxsus",
+#            'tillar':['html','css','js']},
     
-    'hasan':{'familiya':'husanov',
-             'tyil':1999,
-             'malumot':'maxsus',
-             'tillar':['pyhton','php']}}
-for ism,info in hamkasblar.items():
-    print(f"\n{ism.title()} {info['familiya'].title()},"
-          f"{info['tyil']}-yilda tug'ilgan. \n"
-          f"Ma'lumoti: {info['malumot']}.\n"
-          "Quyidagi dasturlash tilllarini biladi:")
-for til in info['tillar']:
-    print(til.upper())
+#    'hasan':{'familiya':'husanov',
+#             'tyil':1999,
+#             'malumot':'maxsus',
+#             'tillar':['pyhton','php']}}
+#for ism,info in hamkasblar.items():
+#    print(f"\n{ism.title()} {info['familiya'].title()},"
+#          f"{info['tyil']}-yilda tug'ilgan. \n"
+#          f"Ma'lumoti: {info['malumot']}.\n"
+#          "Quyidagi dasturlash tilllarini biladi:")
+#for til in info['tillar']:
+ #   print(til.upper())
 
 # AMALIYOT
 mashhurlar={
-    'leonardo di caprio':{"americada tug'ilgan aktyor.Titanic va boshqalarda bosh rol ijrochisi"},
-    'c ronaldo':{'38 yosh portugaliyada tavallud topgan futbolchi},
-    'zaha hadid':['arxitektor','lvianda tugilgan','the best architect'],
-    'eynshteyn':['fizik','atom energiya ixtirochisi','nobel sohibi']}
+    'leonardo di caprio':["americada tug'ilgan aktyor.Titanic va boshqalarda bosh rol ijrochisi"],
+    'elvs presley':['rock yulduzi', 'oscar sohibi', 'grammy sohibi' ],
+    'c ronaldo':['38 yosh portugaliyada tavallud topgan futbolchi'],
+    'zaha hadid':['lvianda tugilgan arxitektor'],
+    'eynshteyn':['fizikadan nobel sohibi']}
 for ism, tariflar in mashhurlar.items():
     print(f"\n{ism.title()}:",end='')
     for tarif in tariflar:
         print(f'{tarif.upper()}',end='')
+
+
+
+nasaf={
+       'shuxrat muhammadiyev':['yarim himoyachi 27 yoshda'],
+       'qobil berdiyev':['darvozabon 22 yoshda osiyo chempioni'],
+       'ruziqul berdiyev':['murabbiy 50 yoshda'],
+       'javohir karimov':['himoyachi 25 yoshda'],
+       'ravshan ermatov':['fifa referesi 48 yoshda'],
+       }
+
+for ism,ishtirokchilar in nasaf.items():
+    print(f"\n{ism.title()}:", end='')
+    for ishtirokchi in ishtirokchilar:
+        print(f'{ishtirokchi.upper()}', end='')
+
+
+
+
+cars={
+      'bmw':['germaniyada ishlab chiqarilgan. X7 mashhur yoltanlamasi'],
+      'toyota':['yapon avtomobil ishlab chiqaruvchisi. Camry rusumi mashhur'],
+      'chevrolet':['amerika avtocompaniyasi va tahoe mashhur rusumi'],
+      'range rover':['ingliz mashinasi va Range rover sport eng masghur'],
+      'mercedes':['nemis mashinasi va Gelendwagen juda mashhur']}
+for rusum, mashinalar in cars.items():
+    print(f"\n{rusum.title()}:", end='')
+    for mashina in mashinalar:
+        print(f'{mashina.upper()}',end='')
+
+
+
 
 
 
