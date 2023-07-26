4db39a56fa4cda44cf04ca959685c9b61632e368
Sherzod <131233831+JohnAleo@users.noreply.github.com>
2023-07-26 21:27:30 +0900

    Update LUG'AT ICHIDA RO'YXAT.py
    aaa

--- LUG'AT ICHIDA RO'YXAT.py
+++ LUG'AT ICHIDA RO'YXAT.py
@@ -45,45 +45,94 @@ Created on Mon Jul 10 20:43:55 2023
  #   print(til.upper())
 
 # AMALIYOT
-mashhurlar={
-    'leonardo di caprio':["americada tug'ilgan aktyor.Titanic va boshqalarda bosh rol ijrochisi"],
-    'elvs presley':['rock yulduzi', 'oscar sohibi', 'grammy sohibi' ],
-    'c ronaldo':['38 yosh portugaliyada tavallud topgan futbolchi'],
-    'zaha hadid':['lvianda tugilgan arxitektor'],
-    'eynshteyn':['fizikadan nobel sohibi']}
-for ism, tariflar in mashhurlar.items():
-    print(f"\n{ism.title()}:",end='')
-    for tarif in tariflar:
-        print(f'{tarif.upper()}',end='')
+#mashhurlar={
+#    'leonardo di caprio':["americada tug'ilgan aktyor.Titanic va boshqalarda bosh rol ijrochisi"],
+#    'elvs presley':['rock yulduzi', 'oscar sohibi', 'grammy sohibi' ],
+#    'c ronaldo':['38 yosh portugaliyada tavallud topgan futbolchi'],
+#    'zaha hadid':['lvianda tugilgan arxitektor'],
+#    'eynshteyn':['fizikadan nobel sohibi']}
+#for ism, tariflar in mashhurlar.items():
+#    print(f"\n{ism.title()}:",end='')
+#    for tarif in tariflar:
+#        print(f'{tarif.upper()}',end='')
 
 
 
-nasaf={
-       'shuxrat muhammadiyev':['yarim himoyachi 27 yoshda'],
-       'qobil berdiyev':['darvozabon 22 yoshda osiyo chempioni'],
-       'ruziqul berdiyev':['murabbiy 50 yoshda'],
-       'javohir karimov':['himoyachi 25 yoshda'],
-       'ravshan ermatov':['fifa referesi 48 yoshda'],
-       }
-
-for ism,ishtirokchilar in nasaf.items():
-    print(f"\n{ism.title()}:", end='')
-    for ishtirokchi in ishtirokchilar:
-        print(f'{ishtirokchi.upper()}', end='')
+#print('space space space space space')
 
 
+#nasaf={
+#       'shuxrat muhammadiyev':['yarim himoyachi 27 yoshda'],
+#       'qobil berdiyev':['darvozabon 22 yoshda osiyo chempioni'],
+#       'ruziqul berdiyev':['murabbiy 50 yoshda'],
+#       'javohir karimov':['himoyachi 25 yoshda'],
+#       'ravshan ermatov':['fifa referesi 48 yoshda'],
+#       }
+
+#for ism,ishtirokchilar in nasaf.items():
+#    print(f"\n{ism.title()}:", end='')
+#    for ishtirokchi in ishtirokchilar:
+#        print(f'{ishtirokchi.upper()}', end='')
+
+#print('space space space space space')
 
 
-cars={
-      'bmw':['germaniyada ishlab chiqarilgan. X7 mashhur yoltanlamasi'],
-      'toyota':['yapon avtomobil ishlab chiqaruvchisi. Camry rusumi mashhur'],
-      'chevrolet':['amerika avtocompaniyasi va tahoe mashhur rusumi'],
-      'range rover':['ingliz mashinasi va Range rover sport eng masghur'],
-      'mercedes':['nemis mashinasi va Gelendwagen juda mashhur']}
-for rusum, mashinalar in cars.items():
-    print(f"\n{rusum.title()}:", end='')
-    for mashina in mashinalar:
-        print(f'{mashina.upper()}',end='')
+#cars={
+#      'bmw':['germaniyada ishlab chiqarilgan. X7 mashhur yoltanlamasi'],
+#      'toyota':['yapon avtomobil ishlab chiqaruvchisi. Camry rusumi mashhur'],
+#      'chevrolet':['amerika avtocompaniyasi va tahoe mashhur rusumi'],
+#      'range rover':['ingliz mashinasi va Range rover sport eng masghur'],
+#      'mercedes':['nemis mashinasi va Gelendwagen juda mashhur']}
+#for rusum, mashinalar in cars.items():
+#    print(f"\n{rusum.title()}:", end='')
+#    for mashina in mashinalar:
+#        print(f'{mashina.upper()}',end='')
+
+#print('space space space space')
+
+#laptops={
+ #   'hp':['amerika brendi pavilion mashhur rusumi'],
+ #   'lg':['Janubiy koreya brendi hammasiyaxshi ishlasa kerak'],
+  #  'samsung':['janubiy koreya brendi hamma laptoplari mashhur'],
+  #  'apple':['amerika brendi mackbook eng mashhur mahsuloti']}
+#for rusumi,laptop in laptops.items():
+#    print(f"\n{rusumi.title()}:",end='')
+ #   for komp in laptop:
+ #       print(f'{komp.upper()}:',end='')
+
+
+
+
+oshnalar={
+    'xumoyun':['action kinolar ishqibozi'],
+    'zayniddin':['kino tomosha qilib uxlab qoladi'],
+    'lazizbek':['tarixiy yoki hujjatli filmlar muhlisi']}
+for oshna, kinolar in oshnalar.items():
+    print(f"\n{oshna.title()}:",end='')
+    for kino in kinolar:
+        print(f'{kino.upper()}',end='')
+    
+
+
+davlatlar={
+    'aqsh':['shimoliy qutbda joylashgan dunyodagi eng qudratli davlat'],
+    'hindiston':['dunyoning eng kop aholiga ega davlati'],
+    'uzbekiston':['dunyoning eng korrupsion davlatlaridan biri'],
+    'rossiya':['hududi boyicha eng katta davlat']}
+for davlat, xususiyatlar in davlatlar.items():
+    print(f"\n{davlat.title()}:",end='')
+    for xususiyat in xususiyatlar:
+        print(f'{xususiyat.upper()}',end='')
+
+
+
+
+
+
+
+
+
+
 
 
 
